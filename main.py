f255a2e2c3ab54d88720a5f0be7211f1af94d4d1
Varyiabol <lyb979428261@gmail.com>
2023-06-25 22:28:05 -0400

    Correct the frontend main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import os
 import streamlit as st
 import model_serving
 
-
 if __name__ == "__main__":
-    dest = 'user_pics'  # this directory is git ignored, contains only processed images.
+    dest = 'user_pics'  # this directory is git ignored, contains only original and processed images.
     if not os.path.exists(dest):
         os.mkdir(dest)
 
@@ -12,6 +11,9 @@ if __name__ == "__main__":
     if upload:
         new_img_name = f"{upload.name.split('.')[0]}-with-keypoints.jpg"
         if st.button('Save File'):
+            # Save the file to a designated place so that we can read later
+            with open(os.path.join(dest, upload.name), "wb") as f:
+                f.write(upload.getbuffer())
             # Predict wheat key points in the uploaded picture
             model_serving.predict_img_svm(
                 img_path=os.path.join(dest, upload.name),
